4fb0d80cd0497ca3cfec46af029d00e809dedd29
Hoanggiang2202 <giangdo.22022004@gmail.com>
2025-11-05 18:16:17 +0700

    hiển thị DANH SÁCH

--- frontend/src/context/PlayerContext.jsx
+++ frontend/src/context/PlayerContext.jsx
@@ -1,20 +1,23 @@
-import React, { createContext, useState } from "react";
+import { createContext, useState, useRef, useEffect } from "react";
 
 export const PlayerContext = createContext();
 
 export const PlayerProvider = ({ children }) => {
   const [playlist, setPlaylist] = useState([]);
   const [currentSong, setCurrentSong] = useState(null);
+  const audioRef = useRef(new Audio());
+
+  useEffect(() => {
+    if (currentSong) {
+      audioRef.current.src = currentSong.url; // URL bài hát
+      audioRef.current.play().catch(err => {
+        console.error("Không phát được nhạc:", err);
+      });
+    }
+  }, [currentSong]);
 
   return (
-    <PlayerContext.Provider
-      value={{
-        playlist,
-        setPlaylist,
-        currentSong,
-        setCurrentSong,
-      }}
-    >
+    <PlayerContext.Provider value={{ playlist, setPlaylist, currentSong, setCurrentSong }}>
       {children}
     </PlayerContext.Provider>
   );
